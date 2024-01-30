515313a6a1cbbfe38fbff81f6a221dc71b6e5bc5
Hiago Gabriel <hiago@gmail.com>
2024-01-30 16:43:04 -0300

    Iniciando estudos de Hooks com react em javascript

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import "./styles/App.css";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Article } from "./components/Article/Article";
+
+import article1Img from "./assets/images/article1.png"
+import article2Img from "./assets/images/article2.png"
+import article3Img from "./assets/images/article3.png"
+import { Counter } from "./components/Counter/Counter";
+// import { Counter } from "./components/Counter/Counter";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Navbar />
+
+        <Counter />
+
+        {/* <section id="articles">
+          <Article title="Designing Dashboards" provider="NASA" description="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet." thumbnail={article1Img}/>
+
+          <Article title="Vibrant Portraits of 2020" provider="SpaceNews" description="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet." thumbnail={article2Img} />
+
+          <Article title="36 Days of Malayalam type" provider="Spaceflight Now" description="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet." thumbnail={article3Img} />
+
+          <Article title="Designing Dashboards" provider="NASA" description="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet." thumbnail={article1Img} />
+
+        </section> */}
+      </>
+    );
+  }
+}
+
+export default App;
